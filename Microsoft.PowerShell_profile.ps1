eaae6b09c067625680c4b34bf04ead2a14ca4c25
Miquel <miquel.parejo@allianz.ie>
2020-02-25 13:03:26 +0000

    added more kubectl alias to powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,14 @@
+Function kgp_function {
+   $a = $args
+   if ($args -eq "."){ $a = $pwd }
+   kubectl get pods $a    
+}
+Function ka_function {
+    $a = $args
+    if ($args -eq "."){ $a = $pwd }
+    kubectl apply $a
+}
+
 set-alias k kubectl
+set-alias kgp kgp_function
+set-alias ka ka_function
